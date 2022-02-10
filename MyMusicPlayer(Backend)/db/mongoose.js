ad03d836b431412f111378eb209fe1fa814e8b39
avdao <76061468+avdao@users.noreply.github.com>
2022-02-10 19:49:16 +0100

    Add files via upload

---
+++ MyMusicPlayer(Backend)/db/mongoose.js
@@ -0,0 +1,7 @@
+
+
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb+srv://avdao:bsphkh123@cluster0.tjxba.mongodb.net/mern_blog?retryWrites=true&w=majority",
+    { useUnifiedTopology: true, useNewUrlParser: true})
